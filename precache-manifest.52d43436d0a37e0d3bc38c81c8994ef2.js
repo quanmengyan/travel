4d5a3c4165dc81311867ffd1b648d5ca8895f202
quan <1206307482@>
2019-01-25 11:21:31 +0800

    travel

---
+++ precache-manifest.52d43436d0a37e0d3bc38c81c8994ef2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fb771ab6db60faf2914",
+    "url": "/css/app.606d0a92.css"
+  },
+  {
+    "revision": "4fb771ab6db60faf2914",
+    "url": "/js/app.52718bd6.js"
+  },
+  {
+    "revision": "f2a96280ef94a3e5b904",
+    "url": "/css/chunk-vendors.b32848a0.css"
+  },
+  {
+    "revision": "f2a96280ef94a3e5b904",
+    "url": "/js/chunk-vendors.fd11bbfc.js"
+  },
+  {
+    "revision": "ee758fdde1de7e7fc1004b6aa917df0d",
+    "url": "/img/01.ee758fdd.png"
+  },
+  {
+    "revision": "19d034c7222737e9a7186cee15589362",
+    "url": "/img/iconfont.19d034c7.svg"
+  },
+  {
+    "revision": "9799c979d3e998f3e6b21869298f7e33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+];
